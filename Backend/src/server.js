4d0ba3874aa72de97344444a78ca28871090538b
OPuder <164337736+OPuder@users.noreply.github.com>
2025-04-14 06:33:57 +0200

    wip

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,1 +1,35 @@
 
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const { createDefaultAdmin } = require("./models/adminSetup");
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+createDefaultAdmin();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Datenbank verbunden"))
+  .catch((err) => console.log("Fehler bei der Verbindung zu MongoDB:", err));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server läuft auf http://localhost:${process.env.PORT}`);
+});
