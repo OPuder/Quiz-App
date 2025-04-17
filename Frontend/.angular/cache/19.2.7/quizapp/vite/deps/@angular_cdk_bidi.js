ae808b3753c37bbb80e4f57fb31e7194b003dba5
OPuder <164337736+OPuder@users.noreply.github.com>
2025-04-17 03:14:53 +0200

    wip

---
+++ Frontend/.angular/cache/19.2.7/quizapp/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,20 @@
+import "./chunk-M3HR6BUY.js";
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-JVDQHSOC.js";
+import "./chunk-VHE76BUB.js";
+import "./chunk-UYUDYMVY.js";
+import "./chunk-EOS55Q6Z.js";
+import "./chunk-WPM5VTLQ.js";
+import "./chunk-PEBH6BBU.js";
+import "./chunk-4S3KYZTJ.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
---
+++ Frontend/.angular/cache/19.2.7/quizapp/vite/deps/@angular_cdk_scrolling.js
@@ -0,0 +1,52 @@
+import "./chunk-UDU42JBG.js";
+import {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory
+} from "./chunk-PLO57S2K.js";
+import "./chunk-LLSYBTIE.js";
+import {
+  Dir
+} from "./chunk-JVDQHSOC.js";
+import "./chunk-5B2PKP7W.js";
+import "./chunk-VHE76BUB.js";
+import "./chunk-UYUDYMVY.js";
+import "./chunk-EOS55Q6Z.js";
+import "./chunk-WPM5VTLQ.js";
+import "./chunk-PEBH6BBU.js";
+import "./chunk-4S3KYZTJ.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory,
+  Dir as ɵɵDir
+};
